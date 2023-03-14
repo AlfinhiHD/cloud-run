734a58045df535e0cfb4a012eb498d17d2817170
AlfinhiHD <85265678+AlfinhiHD@users.noreply.github.com>
2023-03-14 08:47:16 +0700

    small updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Goodbye from Cloud Run!');
+	res.send('Alfinhi Hajid Dhia (123200142)');
 });
 
 const PORT = process.env.PORT || 8080;
